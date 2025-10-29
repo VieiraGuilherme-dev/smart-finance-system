463adcf3fad3edcc69433f80baf2ae2d893bae63
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-29 15:41:03 -0300

    Adding endpoints to the ProdutoController class

--- src/main/java/controller/ProdutoController.java
+++ src/main/java/controller/ProdutoController.java
@@ -1,12 +1,14 @@
 package controller;
 
 import model.Produto;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import service.ProdutoService;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
 
     private final ProdutoService service;
@@ -15,31 +17,38 @@ public class ProdutoController {
         this.service = service;
     }
 
-    public Produto criarProduto(Produto produto) {
+    @PostMapping
+    public Produto criarProduto(@RequestBody Produto produto) {
         return service.salvar(produto);
     }
 
+    @GetMapping
     public List<Produto> listarProdutos() {
         return service.listarTodos();
     }
 
+    @GetMapping("/{id}")
     public Produto buscarProduto(@PathVariable String id) {
         return service.buscarPorId(id).orElse(null);
     }
 
+    @GetMapping("/codigo/{codigo}")
     public Produto buscarPorCodigo(@PathVariable String codigo) {
         return service.buscarPorCodigo(codigo);
     }
 
+    @GetMapping("/categoria/{categoria}")
     public List<Produto> buscarPorCategoria(@PathVariable String categoria) {
         return service.buscarPorCategoria(categoria);
     }
 
+    @PutMapping("/{id}")
     public Produto atualizarProduto(@PathVariable String id, @RequestBody Produto produto) {
         produto.setId(id);
         return service.salvar(produto);
     }
-
+    
+    @DeleteMapping("/{id}")
     public void deletarProduto(@PathVariable String id) {
         service.deletar(id);
     }
