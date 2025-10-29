63d4d4a9494650753e7506af0d6cc9d086fc5417
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-29 17:53:57 -0300

    feat: add Fornecedor entity

---
+++ src/main/java/com/guilherme/inventario/model/Fornecedor.java
@@ -0,0 +1,52 @@
+package com.guilherme.inventario.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "fornecedores")
+public class Fornecedor {
+    private String id;
+    private String nome;
+    private String cnpj;
+    private String contato;
+    private String endereço;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+}
