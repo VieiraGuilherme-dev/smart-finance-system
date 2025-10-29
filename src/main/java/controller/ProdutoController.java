bdbb3ae481984b70ec177d0c0a5d299d8508ce12
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-29 15:31:30 -0300

    Implement ProdutoController

---
+++ src/main/java/controller/ProdutoController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import model.Produto;
+
+public class ProdutoController {
+
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
+
+    public Produto criarProduto(Produto produto) {
+        return service.salvar(produto);
+    }
+
+    public List<Produto> listarProdutos() {
+        return service.listarTodos();
+    }
+
+    public Produto buscarProduto(@PathVariable String id) {
+        return service.buscarPorId(id).orElse(null);
+    }
+
+    public Produto buscarPorCodigo(@PathVariable String codigo) {
+        return service.buscarPorCodigo(codigo);
+    }
+
+    public List<Produto> buscarPorCategoria(@PathVariable String categoria) {
+        return service.buscarPorCategoria(categoria);
+    }
+
+    public Produto atualizarProduto(@PathVariable String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        return service.salvar(produto);
+    }
+
+    public void deletarProduto(@PathVariable String id) {
+        service.deletar(id);
+    }
+}
